4682901f108400465df02c7cc23bf1f87031a67a
Dheeraj chaurasiya <dheerajchaurasiya444@gmail.com>
2023-07-01 21:08:32 +0530

    first changes

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,15 @@
+const Contact = () => {
+    return (
+      <>
+          <h1>Contact Page</h1>
+         <hr />
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit atque ut doloribus unde rem quos molestiae fugit sequi saepe fuga eos magnam, cupiditate voluptates impedit perspiciatis maiores deserunt veniam sit sapiente quis. Rem non illum et officiis voluptate repellendus ea tempora recusandae pariatur, dicta consequatur harum, magni aliquid molestias vero quisquam ratione cumque fugiat maiores minus dolor eaque. Tempore maxime quo saepe debitis voluptatibus. Quos illo ea, eos eius incidunt repellendus suscipit voluptates rerum harum, esse repudiandae. Ex, cum! Earum porro recusandae quis nostrum dolores provident, similique, delectus, 
+          ab doloribus qui quaerat temporibus laborum sapiente fugiat quasi tempora labore ipsum!`</p>
+           {/* <h1>I am teching python</h1> */}
+        
+      </>
+  
+    );
+  }
+  
+  export default Contact;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+const Home = () => {
+    return (
+      <>
+         <h1>Home Page</h1>
+         <hr />
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit atque ut doloribus unde rem quos molestiae fugit sequi saepe fuga eos magnam, cupiditate voluptates impedit perspiciatis maiores deserunt veniam sit sapiente quis. Rem non illum et officiis voluptate repellendus ea tempora recusandae pariatur, dicta consequatur harum, magni aliquid molestias vero quisquam ratione cumque fugiat maiores minus dolor eaque. Tempore maxime quo saepe debitis voluptatibus. Quos illo ea, eos eius incidunt repellendus suscipit voluptates rerum harum, esse repudiandae. Ex, cum! Earum porro recusandae quis nostrum dolores provident, similique, delectus, 
+          ab doloribus qui quaerat temporibus laborum sapiente fugiat quasi tempora labore ipsum!`</p>
+      </>
+    );
+  }
+  
+  export default Home;
+  
